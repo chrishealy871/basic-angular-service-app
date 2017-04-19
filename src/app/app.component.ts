e7d7e0f8ffc00991401b1b0e5528c06ed3f03fc5
Chris Healy <chrishealy871@gmail.com>
2017-04-19 12:23:09 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import { ExampleService } from './example.service';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{title}}</h1>`,
+  providers: [ExampleService]
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  { 
+   
+   title: string;
+
+   constructor( private _exampleService: ExampleService) {
+
+   }
+
+   ngOnInit() {
+        this.title = this._exampleService.someMethod();
+   }
+ }
